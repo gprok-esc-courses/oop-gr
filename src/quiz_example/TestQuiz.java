03359f673acb7c5c2c27e29534eb8d10b4a069ad
gprok <gprok60@gmail.com>
2024-05-14 11:17:01 +0300

    quiz example

---
+++ src/quiz_example/TestQuiz.java
@@ -0,0 +1,12 @@
+package quiz_example;
+
+import org.json.simple.JSONArray;
+
+import java.util.ArrayList;
+
+public class TestQuiz {
+    public static void main(String[] args) {
+        Quiz quiz = new Quiz();
+        quiz.play();
+    }
+}
